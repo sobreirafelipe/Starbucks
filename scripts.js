9e714c663e74c8092995c70b211f752e1eddf22e
sobreirafelipe <sobreira.cf@gmail.com>
2025-03-27 03:33:22 -0300

    git commit -m "Adiciona nova funcionalidade de busca"

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let circle = document.querySelector(".circle")
+
+function trocarcor(cor) {
+    circle.style.background = cor
+}
+let imagem = document.querySelector(".starbucks")
+function trocaimagem(img){
+    imagem.src = img
+}
